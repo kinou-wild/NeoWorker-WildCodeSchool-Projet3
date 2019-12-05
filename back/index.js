83ca670ea239813aeafbe8a9f3f0ac07c3e7eca6
AlexG <alexguidat@gmail.com>
2019-12-05 17:30:29 +0100

    création users delete users findall

--- back/index.js
+++ back/index.js
@@ -3,14 +3,38 @@ const models = require('./models')
 
 const app = express()
 
-app.get('/',(req,res)=>
-models
-.users
-.findAll()
-.then(x=>res.json(x))
+app.get('/', (req, res) =>
+    models
+    .users //mettre la table voulu
+    .findAll()
+    .then(x => res.json(x))
 )
 
+//ajout nouvel user
+app.post('/', (req, res) =>
+    models
+    .users //mettre la table voulu
+    .create({
+        role: "",
+        email: "",
+        password: ""
+    })
+    .then(e => res.json(e)))
+
+//delete user
+app.delete('/:id', (req, res) =>
+    models
+    .users
+    .destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(() => console.log("Utilisateur effacer"))
+)
+
+
 models
-.sequelize
-.sync()
-.then(()=>app.listen(4000, ()=>console.log('server is listenning')))
+    .sequelize
+    .sync()
+    .then(() => app.listen(4000, () => console.log('server is listenning')))
