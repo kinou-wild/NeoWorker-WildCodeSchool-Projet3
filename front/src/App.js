a329226a513412d96688de88ea76692d88a379c9
Anthony Portier <anthonynicolasportier@gmail.com>
2019-12-09 16:13:52 +0100

    ajout li app

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const[dataa,setData] = useState([])
+  const[freelancer,setFreelancer] = useState({
+      img: "",
+      title: "",
+      firstname: "",
+      lastname: "",
+      address: "",
+      mobilite: 4,
+      pref_lieu_de_travail: 2,
+      disponibilite: 1,
+      fourchette_tarifaire: 1,
+      password: "dfgsfd",
+      email: ""
+  })
+  
+  useEffect(() => {
+      fetchData()
+  }, [])
+
+  const fetchData=()=>{
+    axios.get('http://localhost:5000/freelancer/profil/1')
+    .then(res => setData(res.data))
+    .catch((err)=>console.log(err))
+  }
+
   return (
-    <div className="App">
+    <div>
+     {dataa.title}
+      
     </div>
   );
 }
