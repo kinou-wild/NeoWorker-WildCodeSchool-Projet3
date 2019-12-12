897bb929129e37aa959e045a6ef0e5534950ec46
Dylan <dylan.berthier13@gmail.com>
2019-12-12 10:57:13 +0100

    appel du router sans les tests

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import Freelancer from './Freelancer'
-import Users from './Users'
 import Router from './components/Router';
 import './App.css'
 
 function App() {
   
-
   return (
     <div>
-     <p>je suis le app</p>
-     <Freelancer/>
-      <Users/> 
-            <Router />
-
-
+        <Router />
     </div>
   );
 }
-
 
 export default App;
