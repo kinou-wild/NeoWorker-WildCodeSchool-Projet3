93be4042ce34b5e0a9cfe5717a9102c088414a56
Anthony Portier <anthonynicolasportier@gmail.com>
2019-12-12 10:08:34 +0100

    changement app conflits

--- front/src/App.js
+++ front/src/App.js
@@ -1,34 +1,23 @@
 import React from 'react';
-<<<<<<< HEAD
 import Freelancer from './Freelancer'
 import Users from './Users'
-=======
 import Router from './components/Router';
 import './App.css'
->>>>>>> fbc21c39d85145beb66252bcfc2b816ea7e8c54d
 
 function App() {
   
 
   return (
-<<<<<<< HEAD
     <div>
      <p>je suis le app</p>
      <Freelancer/>
       <Users/> 
+            <Router />
+
 
     </div>
   );
 }
 
 
-
 export default App;
-=======
-    <>
-      <Router />
-    </>
-  );
-}
-export default App;
->>>>>>> fbc21c39d85145beb66252bcfc2b816ea7e8c54d
