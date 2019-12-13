8bbe8174d2c1da419c0d20ebebc4ba2570a180f5
Dylan <dylan.berthier13@gmail.com>
2019-12-13 10:39:34 +0100

    .

--- front/src/components/freelancer/EditHomePageFreelancer.jsx
+++ front/src/components/freelancer/EditHomePageFreelancer.jsx
@@ -108,7 +108,7 @@ const EditHomePageFreelancer = (props) => {
               <div className='profil-card'>
             <p className='name-card'>Profil NeoWorker</p>
             <img className='pic-card' src='https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=6&m=476085198&s=612x612&w=0&h=5cDQxXHFzgyz8qYeBQu2gCZq1_TN0z40e_8ayzne0X0=' alt='profil picture'/>
-            </div> 
+           
             <h1 className='freelancer-h1'>Bienvenue sur ton espace personnel</h1>
             <div className='homepage-content'>
                 <img className='pic-profil' src='https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=6&m=476085198&s=612x612&w=0&h=5cDQxXHFzgyz8qYeBQu2gCZq1_TN0z40e_8ayzne0X0=' alt='photo de profil' />
