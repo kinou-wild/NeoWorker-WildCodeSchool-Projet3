1fcf5f4c2ab31743c2aae2e342546ecc543f9d64
Dylan <dylan.berthier13@gmail.com>
2019-12-16 15:39:47 +0100

    Competences Freelancer en no scroll page, ajout du css React-slick dans le HTML, creation de pages Slide pour aller dans le slider

---
+++ front/src/components/freelancer/Slide3.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+
+const Slide3 = () => {
+        //hooks de la data freelancer pour get un id
+        const [idFree, setIdFree] = useState([])
+
+        //hooks de la data user pour le get de l'id
+        const [idUser, setIdUser] = useState([])
+
+return(
+<div>
+
+<h2 className='competences-title'>Centre d'interet</h2>
+<div className='competences'>
+    <div className='competences-card'>
+        <p>sport</p>
+        <p>{String(idFree.sport)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>passion</p>
+        <p>{String(idFree.passion)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>engagement_asso</p>
+        <p>{String(idFree.engagement_asso)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>autres_softskill</p>
+        <p>{String(idFree.autres_softskill)}</p>
+    </div>
+</div>
+
+<h2 className='competences-title'>Langues</h2>
+<div className='competences'>                    
+    <div className='competences-card'>
+        <p>francais</p>
+        <p>{String(idFree.francais)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>anglais</p>
+        <p>{String(idFree.anglais)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>itapen</p>
+        <p>{String(idFree.itapen)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>chinois</p>
+        <p>{String(idFree.chinois)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>russe</p>
+        <p>{String(idFree.russe)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>arabe</p>
+        <p>{String(idFree.arabe)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>allemand</p>
+        <p>{String(idFree.allemand)}</p>
+    </div>
+    <div className='competences-card'>
+        <p>autres_langue</p>
+        <p>{String(idFree.autres_langue)}</p>
+    </div>
+    </div>
+</div>
+
+)
+}
+
+export default Slide3
