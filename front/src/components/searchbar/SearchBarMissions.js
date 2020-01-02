2b5f80c30211af7f15f3f3935917b32dc67c2b45
AlexG <alexguidat@gmail.com>
2020-01-02 15:17:08 +0100

    ajout compo search bar mission et neo

---
+++ front/src/components/searchbar/SearchBarMissions.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import './SearchBarMissions.css'
+
+
+function SearchBarMission() {
+  const [searchTerm, setSearchTerm] = useState({
+        nom:''
+    });
+    const [searchResults, setSearchResults] = useState([]);
+    const[result,setResult]= useState([]);
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = () => {
+        axios.get('http://localhost:5000/missions')
+            .then(res => setSearchResults(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        const results = searchResults.filter(person =>
+            person.modele.toLowerCase().includes(searchTerm.nom));
+            setResult(results);
+    }, [searchTerm.nom]);
+
+
+    return (
+        <div className="search">
+            <input
+                className="search-input"
+                type="text"
+                placeholder="Search"
+                value={searchTerm.nom}
+                onChange={(e) => { setSearchTerm({ ...searchTerm,nom:e.target.value }) }}
+
+            />
+            <ul>
+                {searchTerm.nom.length===0?
+                searchResults.map(item => {
+                    return (
+                        <div>
+                            <li>{item.modele}</li>
+                        </div>)
+                }):
+                result.map(item => {
+                    return (
+                        <div>
+                            <li>{item.modele}</li>
+                        </div>)
+                })}
+
+                
+            </ul>
+        </div>
+    );
+}
+
+export default SearchBarMission
