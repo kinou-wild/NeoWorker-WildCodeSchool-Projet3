33fbe3f9e5fd3100f5f1e63fa158e86cdff6b8be
Dylan <dylan.berthier13@gmail.com>
2020-01-06 10:18:01 +0100

    creation en front de 3 hooks pour get les missions selon leurs status, en back changement de la route get all missions pour récupérer les status

--- back/routes/mission.js
+++ back/routes/mission.js
@@ -1,57 +1,62 @@
 const models = require('../models')
 
-module.exports = function(app){
+module.exports = function (app) {
 
-//Get all mission
-app.get('/missions', (req, res) =>{
-models
-.missions
-.findAll({include : [models.users]})
-.then(x => res.json(x))
-});
+    //Get all mission
+    app.get('/missions', (req, res) => {
+        models
+            .missions
+            .findAll({
+                where: {
+                    status: req.query.status
+                },
+                include: [models.users]
+            })
+            .then(x => res.json(x))
+    });
 
 
-//Get missions by id
-app.get('/mission/:id', (req, res) =>{
-models
-.missions
-.findByPk(req.params.id)
-.then(mission => res.json(mission))
-});
+    //Get missions by id
+    app.get('/mission/:id', (req, res) => {
+        models
+            .missions
+            .findByPk(req.params.id)
+            .then(mission => res.json(mission))
+    });
 
-// Create mission 
-app.post('/missions', (req, res) =>{
-models
-.missions
-.create(req.body)
-    .then(newMission => {
-        newMission.addUser(req.body.userId)
-        res.json(newMission)
-    })
+    // Create mission 
+    app.post('/missions', (req, res) => {
+        models
+            .missions
+            .create(req.body)
+            .then(newMission => {
+                newMission.addUser(req.body.userId)
+                res.json(newMission)
+            })
 
-});
+    });
 
-//UPDATE missions
-app.put('/mission/:id', (req, res) => {
-    models
-    .missions
-    .update(req.body, {
-        where: {
-            id: req.params.id
-        }  
-    }).then(() => console.log("Updated"))
-});
+    //UPDATE missions
+    app.put('/mission/:id', (req, res) => {
+        models
+            .missions
+            .update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            }).then(() => console.log("Updated"))
+    });
 
-//Delete missions
-app.delete('/mission/:id', (req, res) =>{
-models
-.missions
-.destroy({
-    where: {
-        id: req.params.id
-    }
-}) .then(() => console.log("Deleted"))
-});
+    //Delete missions
+    app.delete('/mission/:id', (req, res) => {
+        models
+            .missions
+            .destroy({
+                where: {
+                    id: req.params.id
+                }
+            }).then(() => console.log("Deleted"))
+    });
 
 
 };
--- front/src/components/admin/MissionsListe.jsx
+++ front/src/components/admin/MissionsListe.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useEffect,useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios'
 import { Link } from 'react-router-dom';
 import { SidebarContext } from '../SidebarContext'
@@ -8,64 +8,73 @@ import MissionDisplayer from "./MissionsListeCard"
 
 
 const MissionsListe = () => {
-    
+
     // hooks to get all missions
-    const [missions, setMissions] = useState([])
+    const [missionsAPourvoir, setMissionsAPourvoir] = useState([])
+    const [missionsPourvues, setMissionsPourvues] = useState([])
+    const [missionsTerminées, setMissionsTerminées] = useState([])
 
     const { hook, hook2 } = useContext(SidebarContext)
     const [showSidebar, setShowSidebar] = hook
     const [roleSidebar, setRoleSidebar] = hook2
 
     useEffect(() => {
-        getMissions() 
+        getMissions()
         setShowSidebar(true)
         setRoleSidebar("admin")
     }, [])
 
     // get all misions
     const getMissions = () => {
-        axios.get('http://localhost:5000/missions')
-            .then(response => setMissions(response.data))
+        axios.get('http://localhost:5000/missions?status=0')
+            .then(response => setMissionsAPourvoir(response.data))
+            .catch((err) => console.log(err))
+        axios.get('http://localhost:5000/missions?status=1')
+            .then(response => setMissionsPourvues(response.data))
+            .catch((err) => console.log(err))
+        axios.get('http://localhost:5000/missions?status=2')
+            .then(response => setMissionsTerminées(response.data))
             .catch((err) => console.log(err))
     }
+
 
     const fetchDeleteDataMission = (id) => {
         axios.delete(`http://localhost:5000/mission/${id}`)
             .catch((err) => console.log(err))
-            window.location.reload(false);
+        window.location.reload(false);
     }
 
     var tempDate = new Date();
-    var date = tempDate.getFullYear() + '-' + (tempDate.getMonth()+1) + '-' + tempDate.getDate() +' '+ tempDate.getHours()+':'+ tempDate.getMinutes()+':'+ tempDate.getSeconds();
+    var date = tempDate.getFullYear() + '-' + (tempDate.getMonth() + 1) + '-' + tempDate.getDate() + ' ' + tempDate.getHours() + ':' + tempDate.getMinutes() + ':' + tempDate.getSeconds();
 
-    return(
-        
+    return (
+
         <div className="admin-mission">
-        <div className='profil-card'>
-            <p className='name-card'>Profil Admin</p>
-            <img className='pic-card' src='https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=6&m=476085198&s=612x612&w=0&h=5cDQxXHFzgyz8qYeBQu2gCZq1_TN0z40e_8ayzne0X0=' alt='profil picture' />
-        </div>
-        <h2 className='missions-h2'>Missions à pourvoir <span className='textModif'>:</span></h2>
-        <div className='missions'>
-             {missions.filter(mission=> mission.status === 0).map(mission => {
-                 return <MissionDisplayer {...mission}/>
-             })}
-         </div>
-        <h2 className='missions-h2'>Missions Pourvues <span className='textModif'>:</span></h2>
-        <div className='missions'>
-            {missions.filter(mission=> mission.status === 1).map(mission => {
-                return <MissionDisplayer {...mission}/>
-            })
-}
-        </div>
-        <h2 className='missions-h2'>Missions terminées <span className='textModif'>:</span></h2>
-        <div className='missions'>
-            {missions.filter(mission=>mission.status === 2).map(mission => {
-                return <MissionDisplayer {...mission}/>
-            })
-        }
-        </div>
-            </div>)
+            <div className='profil-card'>
+                <p className='name-card'>Profil Admin</p>
+                <img className='pic-card' src='https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?k=6&m=476085198&s=612x612&w=0&h=5cDQxXHFzgyz8qYeBQu2gCZq1_TN0z40e_8ayzne0X0=' alt='profil picture' />
+            </div>
+            <h2 className='missions-h2'>Missions à pourvoir <span className='textModif'>:</span></h2>
+            <div className='missions'>
+                {missionsAPourvoir.map(mission => {
+                    return <MissionDisplayer {...mission} />
+                })}
+            </div>
+            <h2 className='missions-h2'>Missions Pourvues <span className='textModif'>:</span></h2>
+            <div className='missions'>
+                {missionsPourvues.map(mission => {
+                    return <MissionDisplayer {...mission} />
+                })
+                }
+            </div>
+            <h2 className='missions-h2'>Missions terminées <span className='textModif'>:</span></h2>
+            <div className='missions'>
+                {missionsTerminées.map(mission => {
+                    return <MissionDisplayer {...mission} />
+                })
+                }
+            </div>
+        </div>)
 }
 
 export default MissionsListe;
