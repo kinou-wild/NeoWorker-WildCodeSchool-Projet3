db82e97595f0ae789478944ce363c55497dd023c
AlexG <alexguidat@gmail.com>
2020-01-07 13:26:58 +0100

    git merge

--- front/src/components/admin/Creation_NeoWorker_Page.jsx
+++ front/src/components/admin/Creation_NeoWorker_Page.jsx
@@ -107,7 +107,7 @@ const CreationNeoWorkerPage = (props) => {
 
         assistance_suivi_comptable: false,
         relation_accountant: false,
-        customer_recovery: DataTypes.BOOLsEAN,
+        customer_recovery: DataTypes.BOOLEAN,
         payroll_preparation: false,
         assembly_approval_file: false,
         referencing_training_organization:false,
