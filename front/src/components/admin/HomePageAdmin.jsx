7ade3fc7a75755d57d0138b01a9f60efab1728de
Dylan <dylan.berthier13@gmail.com>
2020-01-09 17:08:44 +0100

    styled homepage and adding some pics

--- front/src/components/admin/HomePageAdmin.jsx
+++ front/src/components/admin/HomePageAdmin.jsx
@@ -1,6 +1,8 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import './HomePageAdmin.css'
 import profilPic from '../../img/anais.jpg'
+import homePic from '../../img/landpic.png'
+import heart from '../../img/heart.png'
 
 /* -------- Page d'accueil de la page Administrateur, après connexion ------------------ */
 const HomePageAdmin = () => {
@@ -12,6 +14,8 @@ const HomePageAdmin = () => {
                 <img className='pic-card' src={profilPic} alt='profil picture' />
             </div>
             <p className="admin-homepage-title">Bienvenue dans l'espace <span className="admin-homepage-orange"> Admin</span> NeoWorker<span className="admin-homepage-orange">.</span></p>
+            <img className='home-pic' src={homePic} alt='draw' />
+            <p className='made'> Made with <img className='heart' src={heart} alt='heart' /></p>
         </div>
     )
 }
