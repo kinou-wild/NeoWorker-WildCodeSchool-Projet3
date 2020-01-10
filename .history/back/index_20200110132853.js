4c36e3f464631f4429b430d7646bbba4ef7a4c5d
Stevie Cutman <steviecut@hotmail.fr>
2020-01-10 13:31:25 +0100

    trying to fix problem

---
+++ .history/back/index_20200110132853.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const models = require('./models')
+const cors = require('cors')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))// pour les params
+app.use(bodyParser.json())
+
+//les root sont dans le dossier back/root/admin ou user
+
+
+require('./routes/freelancer.js')(app)
+require('./routes/user.js')(app)
+require('./routes/mission.js')(app)
+
+models
+    .sequelize
+    .sync({force : false})
+    .then(() => app.listen(5000, () => console.log('server is listenning')))
