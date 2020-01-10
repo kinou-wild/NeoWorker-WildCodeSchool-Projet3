d3e64f5d4c850e8c77f882f89dd91fff1dad07a3
kinou-wild <kevin.thomann13@gmail.com>
2020-01-10 15:00:40 +0100

    minor bug fixed

--- front/src/components/admin/NeoworkerList.jsx
+++ front/src/components/admin/NeoworkerList.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import NeoworkerListCard from './NeoWorkerListCard'
-import './NeoworkerList.css'
+import './ListeNeoworker&Mission.css'
 
 const NeoworkerList = () => {
 
