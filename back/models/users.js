541c01f088fd3b2cc8331d4064f0f2d98de19860
Anthony Portier <anthonynicolasportier@gmail.com>
2020-01-13 19:54:15 +0100

    secu back ok avec sequelize

--- back/models/users.js
+++ back/models/users.js
@@ -1,10 +1,39 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const users = sequelize.define('users', {
-    role: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+    role: {
+      type: DataTypes.STRING,
+      validate: {
+        isIn: [
+          ['admin', 'neoworker']
+        ], // check the value is one of these
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [0, 40],
+        isEmail: true,
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [0, 60],
+      }
+
+    },
+  }, {
+    hooks:{
+      beforeCreate:(user)=> {
+        if (user.dataValues.role === 'admin' || user.dataValues.role==='neoworker'){///supp le admin à la fin du projet
+          return user
+        }else{
+        return sequelize.Promise.reject()}
+      },
+
+    }
+  });
   users.associate = function(models) {
     users.belongsToMany(models.freelancer,{
         through: 'tj_users_f'
