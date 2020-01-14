a8eeb94ae39a4328fc307e35b32a6530426ecc8c
AlexG <alexguidat@gmail.com>
2020-01-14 13:42:52 +0100

    ajout link et composant MatchingPage

---
+++ front/src/components/matching/MatchingPage.jsx
@@ -0,0 +1,14 @@
+import React, {useReducer} from 'react';
+import './MatchingPage.css'
+
+
+const MatchingPage = () => {
+    return ( 
+        <div className="container-matching">
+            <h1>welcome to the
+                 matching page</h1>
+        </div>
+     );
+}
+ 
+export default MatchingPage;
